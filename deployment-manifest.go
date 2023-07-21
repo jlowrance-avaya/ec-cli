efd88dde33185462c0e96c1c6b13460c8ab1abc2
Josh Lowrance <$WORK_EMAIL>
2023-07-21 09:21:12 -0500

    fix: try http.request on deploymentManifest

--- deployment-manifest.go
+++ deployment-manifest.go
@@ -1,12 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 type DeploymentManifest struct {
 }
 
 func getDeploymentManifest(api *API, id string) {
-	fmt.Println("getDeploymentManifest...")
+	http.HandleFunc("/api/deployment_manifest/", func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Authorization header required", http.StatusUnauthorized)
+			return
+		}
+
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			http.Error(w, "Invalid Authorization header", http.StatusUnauthorized)
+			return
+		}
+
+		token := parts[1]
+
+		// Here you would validate the token to make sure it's valid. For simplicity,
+		// we'll just check that it's a specific value.
+		if token != api.Token {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		id := r.URL.Path[len("/api/deployment_manifest/"):]
+
+		// Here you would typically fetch the deployment manifest from your data store
+		// based on the id. For simplicity, we'll just return a static string.
+
+		fmt.Fprintf(w, "You requested deployment manifest: %s", id)
+	})
+
+	http.ListenAndServe(fmt.Sprintf("%s:8080", api.Endpoint), nil)
 }
 
 func getDeploymentManifests(api *API) {
--- go.mod
+++ go.mod
@@ -2,17 +2,12 @@ module ec-cli
 
 go 1.20
 
-require (
-	github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos v0.3.5
-	github.com/alecthomas/kingpin v2.2.6+incompatible
-)
+require github.com/alecthomas/kingpin v2.2.6+incompatible
 
 require (
-	github.com/Azure/azure-sdk-for-go v68.0.0+incompatible // indirect
-	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.6.1 // indirect
-	github.com/Azure/azure-sdk-for-go/sdk/internal v1.3.0 // indirect
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
